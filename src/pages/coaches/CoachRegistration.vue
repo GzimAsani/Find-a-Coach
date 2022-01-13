3797b0d80e125f023cf63c6de91817417f5107bf
GzimAsani <gzimasanni@gmail.com>
2022-01-13 13:27:22 +0100

    Created coachRegistration form and added to the Coach registration

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Register as a coach!</h1>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: { CoachForm },
+};
+</script>
