f90e09296d0d1fc99a85f7f37762d596cb2eae02
GzimAsani <gzimasanni@gmail.com>
2022-01-13 11:23:11 +0100

    Created coachFilter and integrated in the coachList

--- src/pages/coaches/CoachList.vue
+++ src/pages/coaches/CoachList.vue
@@ -1,5 +1,7 @@
 <template>
-  <section>FILTER</section>
+  <section>
+    <coaches-filter @change-filter="setFilters"></coaches-filter>
+  </section>
 
   <section>
     <base-card>
@@ -25,15 +27,41 @@
 
 <script>
 import CoachItem from '../../components/coaches/CoachItem.vue';
-import BaseButton from '../../components/ui/BaseButton.vue';
+import CoachesFilter from '../../components/coaches/CoachFilter.vue';
 export default {
-  components: { CoachItem, BaseButton },
+  data() {
+    return {
+      activeFilters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+    };
+  },
+  components: { CoachItem, CoachesFilter },
   computed: {
     filteredCoaches() {
-      return this.$store.getters['coaches/coaches'];
+      const coaches = this.$store.getters['coaches/coaches'];
+      return coaches.filter((coach) => {
+        if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
+          return true;
+        }
+        if (this.activeFilters.backend && coach.areas.includes('backend')) {
+          return true;
+        }
+        if (this.activeFilters.career && coach.areas.includes('career')) {
+          return true;
+        }
+        return false;
+      });
     },
     hasCoaches() {
       return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+  methods: {
+    setFilters(updatedFilters) {
+      this.activeFilters = updatedFilters;
     },
   },
 };
