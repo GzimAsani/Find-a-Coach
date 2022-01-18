a4ce0d4040bb197ac0988b6e7eb5b624c53a2202
GzimAsani <gzimasanni@gmail.com>
2022-01-18 09:37:32 +0100

    Send a PUT http request to store coach data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,14 +1,31 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
-      areas: data.areas
+      areas: data.areas,
     };
 
-    context.commit('registerCoach', coachData);
-  }
+    const response = await fetch(
+      `https://vue-coach-app-13dd0-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      //throw an error
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId,
+    });
+  },
 };
