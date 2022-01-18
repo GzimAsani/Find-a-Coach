e5b8f4de07cb2dff5746b875421a9414b470938a
GzimAsani <gzimasanni@gmail.com>
2022-01-18 09:55:06 +0100

    Fetch coach data(GET Http requests)

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -28,4 +28,29 @@ export default {
       id: userId,
     });
   },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://vue-coach-app-13dd0-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      //Throw an error
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+    context.commit('setCoaches', coaches);
+  },
 };
